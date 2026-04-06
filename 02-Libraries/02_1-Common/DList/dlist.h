564c566bd5fb1bee60fdbb5aca9df6f5c0a560e2
renyerenyeren <13697394398@163.com>
2026-04-06 15:27:27 +0800

    feat(DList): 添加双向链表数据结构实现
    - 引入stdint.h标准整型定义
    - 定义DLIST_MAX_NODES宏控制最大节点数量
    - 创建dlist_status_t枚举定义操作状态码
    - 实现dnode_t结构体定义双向链表节点
    - 创建dlist_t结构体定义链表管理结构
    - 添加链表初始化、清空、插入、删除功能接口
    - 实现按值和按位置查找节点功能
    - 提供正向反向遍历及链表反转接口

--- 02-Libraries/02_1-Common/DList/dlist.h
+++ 02-Libraries/02_1-Common/DList/dlist.h
@@ -6,24 +6,115 @@
 #define RTOS_PROJECT_DLIST_H
 
 //******************************** Includes *********************************//
+#include <stdint.h>
 //******************************** Includes *********************************//
 //---------------------------------------------------------------------------//
 //******************************** Defines **********************************//
+#define DLIST_MAX_NODES    20      // 每个链表的最大节点数（可配置）
 //******************************** Defines **********************************//
 //---------------------------------------------------------------------------//
 //******************************** Typedefs *********************************//
+typedef enum
+{
+    DLIST_OK         = 0,// 成功
+    DLIST_ERR_NULL,      // 空指针
+    DLIST_ERR_FULL,      // 节点池已满
+    DLIST_ERR_POS,       // 非法位置
+    DLIST_ERR_NOT_FOUND  // 未找到节点
+} dlist_status_t;
 //******************************** Typedefs *********************************//
 //---------------------------------------------------------------------------//
 //**************************** Interface Structs ****************************//
+typedef struct dnode
+{
+    void *data;
+    struct dnode *next;
+    struct dnode *prev;
+}dnode_t;
 //**************************** Interface Structs ****************************//
 //---------------------------------------------------------------------------//
 //******************************** Classes **********************************//
+typedef struct
+{
+    dnode_t nodePool[DLIST_MAX_NODES];
+    dnode_t *freeList;
+    dnode_t *head;
+    dnode_t *tail;
+    uint32_t length;
+    uint32_t maxNodes;
+} dlist_t;
 //******************************** Classes **********************************//
 //---------------------------------------------------------------------------//
 //**************************** Extern Variables *****************************//
 //**************************** Extern Variables *****************************//
 //---------------------------------------------------------------------------//
 //******************************** 函数声明 ***********************************//
+/**
+ * @brief  初始化链表实例（必须先调用）
+ * @param  list: 链表实例指针
+ * @return 状态码
+ */
+dlist_status_t dListInit(dlist_t* list);
+
+/**
+ * @brief  清空链表（自动释放data，若传入析构函数）
+ */
+void dListClear(dlist_t* list);
+
+/**
+ * @brief  头部插入
+ */
+dlist_status_t dListInsertHead(dlist_t* list, void* data);
+
+/**
+ * @brief  尾部插入
+ */
+dlist_status_t dListInsertTail(dlist_t* list, void* data);
+
+/**
+ * @brief  按位置插入（0=头部）
+ */
+dlist_status_t dListInsertAt(dlist_t* list, uint32_t pos, void* data);
+
+/**
+ * @brief  删除指定节点
+ */
+dlist_status_t dListDeleteNode(dlist_t* list, dnode_t* node);
+
+/**
+ * @brief  按值删除（第一个匹配项）
+ */
+dlist_status_t dListDeleteByValue(dlist_t* list, void* data);
+
+/**
+ * @brief  按位置删除
+ */
+dlist_status_t dListDeleteAt(dlist_t* list, uint32_t pos);
+
+/**
+ * @brief  按值查找节点
+ */
+dnode_t* dListFindByValue(dlist_t* list, void* data);
+
+/**
+ * @brief  按位置查找节点
+ */
+dnode_t* dListFindByPosition(dlist_t* list, uint32_t pos);
+
+/**
+ * @brief  正向遍历
+ */
+void dListForEachForward(dlist_t* list, void* userData);
+
+/**
+ * @brief  反向遍历
+ */
+void dListForEachBackward(dlist_t* list, void* userData);
+
+/**
+ * @brief  反转链表
+ */
+void dListReverse(dlist_t* list);
 //******************************** 函数声明 ***********************************//
 
 #endif //RTOS_PROJECT_DLIST_H
