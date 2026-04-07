a8c85ab441f888d1667c2437c6c92e5bbe796452
renyerenyeren <13697394398@163.com>
2026-04-07 12:07:10 +0800

    feat(ST7789): 添加系统适配层接口和初始化函数

--- 03-Adapter/03_4-ST7789/st7789_system_adaption.h
+++ 03-Adapter/03_4-ST7789/st7789_system_adaption.h
@@ -5,4 +5,10 @@
 #ifndef RTOS_PROJECT_ST7789_SYSTEM_ADAPTION_H
 #define RTOS_PROJECT_ST7789_SYSTEM_ADAPTION_H
 
+#include "st7789_driver.h"
+
+extern bsp_st7789_driver_t st7789_driver_instance;
+
+void st7789_system_adaption(void);
+
 #endif //RTOS_PROJECT_ST7789_SYSTEM_ADAPTION_H
